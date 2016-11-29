c14846af245de7120d96f85705892a101bafea20
ayoub.korkot <ayoub.korkot@gmail.com>
2016-11-29 22:00:25 +0000

    modif formationphp

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN">
+<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 
 <head>
 
@@ -15,10 +15,49 @@
 
 <!-- affecter des informations dans une variable -->
 <?php 
-	$toto_en = '<p style="color:red">HELLO WORLD</p>';
-	$toto_fr = '<p style="color:green">BONJOUR LE MONDE</p>';
-	$toto_langue = 'toto_fr';
+
+	//Declarer une constante 
+	define('AFFICHE', 'fr');
+
+
+
+		/*----------------------------------*/
+	/*
+		$toto_en = '<p style="color:red">HELLO WORLD</p>';
+		$toto_fr = '<p style="color:green">BONJOUR LE MONDE</p>';
+		$toto_langue = 'toto_fr';
+		$retourligne = '<br />';
+	*/
+		/*----------------------------------*/
+
+
+	//une autre manière de déclarer puis de remplir un tableau
+	/*	
+		$langue = array();
+		$langue['en'] = '<p style="color:red">HELLO WORLD</p>';
+		$langue['fr'] = '<p style="color:green">BONJOUR LE MONDE</p>';
+	*/
+
+		/*----------------------------------*/
+
+
+	$langue = array(
+		'en'=>'<p style="color:red">HELLO WORLD</p>',
+		'fr'=>'<p style="color:green">BONJOUR LE MONDE</p>'
+		);
+
+
+	$affichage = 'en';
 	$retourligne = '<br />';
+
+	//délimiter une chaine de caratère avec la syntaxe heredoc, en donnant un identifient
+	/*	$toto = <<<EOT
+		je suis content d'apprendre le php ! 
+		La langue affichée est stockée dans la variable $affichage
+	EOT
+	*/
+
+
 ?>
 
 
@@ -26,7 +65,7 @@
 <!-- Afficher le contenu de la variable-->
 <?php
 
-	echo $$toto_langue;
+		/*echo $$toto_langue;
 		$toto = 1;
 		echo $toto;
 		echo $retourligne;
@@ -38,35 +77,44 @@
 		echo $retourligne;
 		$toto = array('tableau1','tableau2');
 		echo $toto[0];
-		echo $retourligne;
+		echo $retourligne;*/
 
 		/* print_r : permet d'afficher une representation textuelle de la structure du tableau toto*/
-		print_r($toto);
+		/*print_r($toto);
 		echo $retourligne;
+		*/
+		/*------------------------------------------------------------*/
+
+	 	/*declaration d'un objet*/
+	 	/*
+	 	class obj {
+
+	 		//passer des propriétés a cet objet
+	 		public $var = 'bonjour';
+	 		private $hidden = 'Non';
+	 		function oui() {
+	 			echo $this->hidden;
+	 		}
+	 	}
+	 	*/
+
+	 	//instantiation de l'objet
+	 		/*$toto = new obj();
+	 		echo $toto->var;*/
+
+	 		//la propriété privé ($hidden) n'est pas accessible en dehors de l'objet lui meme
+	 		/*echo $toto->hidden;
+	 		$toto->oui();
+	 		*/
+
+	 	/*------------------------------------------------------------*/
+
+	 	echo $langue[AFFICHE];
+	 	echo $retourligne;
+
 
  ?>
 
- <?php
-
- 	/*declaration d'un objet*/
- 	class obj {
- 		/*passer des propriétés a cet objet*/
- 		public $var = 'bonjour';
- 		private $hidden = 'Non';
- 		function oui() {
- 			echo $this->hidden;
- 		}
- 	}
-
- 	/*instantiation de l'objet*/
- 		$toto = new obj();
- 		echo $toto->var;
-
- 		/*la propriété privé ($hidden) n'est pas accessible en dehors de l'objet lui meme*/
- 		/*echo $toto->hidden;*/
-
- 		$toto->oui();
- ?>
 
 
 
